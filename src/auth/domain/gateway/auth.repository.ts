d45b2723afb573484bc59cf09e8cd27b02fcf657
Jorge Martin Herrera Tume <jherrerat-ext@dinersclub.com.pe>
2024-03-13 15:09:55 -0500

    fix: with first commit remove auth

--- src/auth/domain/gateway/auth.repository.ts
+++
@@ -1,7 +0,0 @@
-import { AuthModel } from "../model/auth/auth.model";
-import { AuthenticateUserDto } from "src/auth/application/dto/auth.dto";
-
-
-export interface AuthRepository {
-    authenticate(account: AuthenticateUserDto): Promise<AuthModel>;
-}
